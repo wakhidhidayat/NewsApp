a719dc38f45f751abb69e32835a0a02fc4f6e432
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-28 22:00:02 +0700

    first commit

---
+++ News App/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Wahid Hidayat on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
