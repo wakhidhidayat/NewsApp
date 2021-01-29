57eb222ee5c89ad0873448bbedf5f786b0ddbafb
Wahid Hidayat <wshidayat38@gmail.com>
2021-01-29 13:37:00 +0700

    added show news

--- News App/Controllers/NewsTableViewCell.swift
+++ News App/Controllers/NewsTableViewCell.swift
@@ -32,13 +32,50 @@ class NewsTableViewCell: UITableViewCell {
     }
     
     func configure(with model: News) {
+        self.poster.layer.cornerRadius = 10
+        self.poster.clipsToBounds = true
+        
         self.title.text = model.title
-        self.date.text = model.publishedAt
+        
+        if let dateTime = model.publishedAt.convertDateString() {
+            self.date.text = "\u{2022} \(dateTime.timeAgoDisplay())"
+        }
+        
         self.source.text = model.source.name
         
         if let data = try? Data(contentsOf: URL(string: model.urlToImage)!) {
             self.poster.image = UIImage(data: data)
         }
     }
-    
 }
+
+extension String {
+    func convertDateString() -> Date? {
+        return convert(dateString: self, fromDateFormat: "yyyy-MM-dd'T'HH:mm:ssZ", toDateFormat: "dd-MM-yyyy")
+    }
+
+    func convert(dateString: String, fromDateFormat: String, toDateFormat: String) -> Date? {
+
+        let fromDateFormatter = DateFormatter()
+        fromDateFormatter.dateFormat = fromDateFormat
+
+        if let fromDateObject = fromDateFormatter.date(from: dateString) {
+
+            let toDateFormatter = DateFormatter()
+            toDateFormatter.dateFormat = toDateFormat
+
+            return fromDateObject
+        }
+
+        return nil
+    }
+}
+
+extension Date {
+    func timeAgoDisplay() -> String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter.localizedString(for: self, relativeTo: Date())
+    }
+}
+
--- News App/Controllers/ViewController.swift
+++ News App/Controllers/ViewController.swift
@@ -7,15 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var newsTable: UITableView!
     
+    var newsDatas = [News]()
+    var activityIndicator: UIActivityIndicatorView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        newsTable.backgroundView = activityIndicator
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "News"
+        fetchNews()
+        
+        newsTable.register(NewsTableViewCell.nib(), forCellReuseIdentifier: NewsTableViewCell.identifier)
+        newsTable.delegate = self
+        newsTable.dataSource = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsDatas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as! NewsTableViewCell
+        cell.configure(with: newsDatas[indexPath.row])
+        return cell
+    }
+    
+    func fetchNews() {
+        activityIndicator.startAnimating()
+        newsTable.separatorStyle = .none
 
-
+        URLSession.shared.dataTask(with: URL(string: "https://newsapi.org/v2/top-headlines?country=id&apiKey=2169e45c9cec490b9aaed732d6090a7b")!, completionHandler: { data, response, error in
+            
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            var result: NewsResult?
+            
+            do {
+                result = try JSONDecoder().decode(NewsResult.self, from: data)
+            } catch {
+                print("Error when parsing news")
+            }
+            
+            guard let finalResult = result else {
+                return
+            }
+            
+            let articles = finalResult.articles
+            self.newsDatas.append(contentsOf: articles)
+            
+            
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.newsTable.separatorStyle = .singleLine
+                self.newsTable.reloadData()
+            }
+        }).resume()
+    }
 }
-
